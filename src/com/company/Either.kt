0c5a5d9ada0bf63befb468b8320412e5183c354e
vladimir <ferylev@list.ru>
2021-05-12 00:43:24 +0300

    either 2

--- src/com/company/Either.kt
+++ src/com/company/Either.kt
@@ -11,7 +11,9 @@ sealed class Either<out L, out R> {
     class Right<R>(val value: R) : Either<Nothing, R>()
 
     // inline - тело функции будет встроено в месте вызова. Не будет создан дополнительный объект передаваемых функций
-    //
+    // fold - функция высшего порядка, так как принимает в качестве параметров функции
+    // передаются две полноценные функции для случаев успеха/неудачи или других состояний, но вызывается только одна
+    // в зависимости от того у какого типа вызываем Left или Right
     inline fun <T> fold(fl: (L) -> T, fr: (R) -> T): T = when (this) {
         is Left -> fl(this.value)
         is Right -> fr(this.value)
@@ -35,14 +37,21 @@ sealed class Either<out L, out R> {
             is Right -> "Right(${value})"
         }
 
+    // companion object - это вроде статических членов в Java. По имени класса Either можно вызывать данные функции
     companion object {
 
+        // принимает функцию, которая ничего не принимает, но возвращает тип Right (успех)
+        // вызываем catch и передаем функцию, которая возвращает то же самое значение что и приняла
+        // и функцию, которая ничего не принимает и возвращает объект типа при успехе
         inline fun <R> catch(fr: () -> R): Either<Throwable, R> =
             catch(::identity, fr)
 
+        // то же самое, только suspend
         suspend inline fun <R> catchSuspend(fr: () -> R): Either<Throwable, R> =
             catchSuspend(::identity, fr)
 
+        // пробуем выполнить функцию fr(), если получается то возвращаем Right(value), если нет и выбрасывается исключение
+        // то возвращаем Left(exception), exception передали в функции fl
         inline fun <L, R> catch(fl: (Throwable) -> L, fr: () -> R): Either<L, R> =
             try {
                 Right(fr())
@@ -59,6 +68,23 @@ sealed class Either<out L, out R> {
     }
 }
 
+// функция расширения класса Either. Параметризуется тремя типами, которые берутся из аргументов
+/*
+Either<HttpException, List<IsupChannel>> = Either<HttpException, InfoApiResponse>.map { response -> response.data
+                                            .filterIsInstance<IsupChannelEntity>()
+                                            .map { it.toStruct() }
+                                            }
+
+
+Either<Throw, upChannel>.flatMap { isupChannels ->
+            when (batch) {
+                false -> isupChannels.right()
+                else -> getCardTrunkHdlcsForIsupChannels(endpoint, isupChannels)
+                        .map { isupChannels + it }
+            }
+        }
+*/
+
 inline fun <L, R, NR> Either<L, R>.flatMap(fr: (R) -> Either<L, NR>): Either<L, NR> =
     flatMapRight(fr)
 
@@ -72,6 +98,13 @@ inline fun <L, R, NL> Either<L, R>.flatMapLeft(fl: (L) -> Either<NL, R>): Either
     is Either.Right -> this
 }
 
+/*
+Преобразовать правую часть Either, передаем функцию преобразвания
+Either<HttpException, List<IsupChannel>> = Either<HttpException, InfoApiResponse>.map { response -> response.data
+    .filterIsInstance<IsupChannelEntity>()
+            .map { it.toStruct() }
+}
+*/
 inline fun <L, R, NR> Either<L, R>.map(fr: (R) -> NR): Either<L, NR> =
     mapRight(fr)
 
